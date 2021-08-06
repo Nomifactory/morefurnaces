bf155db92217b4407ed5a69e288f46eaacdc5fcb
Dane Strandboge <dane@strandboge.com>
2021-08-06 01:54:26 -0500

    Drop CXLib Dependency, fix various GUI bugs (#5)
    Fixes cubex2#7 cubex2#14 (possibly others as well, like missing inventory drop shadow)
    
    * Switch to GTCE ForgeGradle fork and latest 1.12.2 MCP mappings
    * Rewrite GUI code to no longer require CXLib at all. Drop dependency.
    * Fix textures
    * Reformat code to standardize style
    * Add TOP to dev environment

---
+++ src/main/java/dan/morefurnaces/gui/Textures.java
@@ -0,0 +1,15 @@
+package dan.morefurnaces.gui;
+
+import dan.morefurnaces.MoreFurnaces;
+import net.minecraft.util.ResourceLocation;
+
+public class Textures {
+
+    public static final ResourceLocation IRON = new ResourceLocation(MoreFurnaces.MODID, "textures/gui/iron.png");
+    public static final ResourceLocation GOLD = new ResourceLocation(MoreFurnaces.MODID, "textures/gui/gold.png");
+    public static final ResourceLocation DIAMOND = new ResourceLocation(MoreFurnaces.MODID, "textures/gui/diamond.png");
+    public static final ResourceLocation NETHERRACK = new ResourceLocation(MoreFurnaces.MODID, "textures/gui/netherrack.png");
+    public static final ResourceLocation OBSIDIAN = new ResourceLocation(MoreFurnaces.MODID, "textures/gui/obsidian.png");
+    public static final ResourceLocation COPPER = new ResourceLocation(MoreFurnaces.MODID, "textures/gui/copper.png");
+    public static final ResourceLocation SILVER = new ResourceLocation(MoreFurnaces.MODID, "textures/gui/silver.png");
+}
